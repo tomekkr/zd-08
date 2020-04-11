595347f986cdbaaa729322e1dfd9c02e54dfe05f
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-11 13:07:25 +0200

    created tv object tested showStatus, turnOn, turnOff methods

--- src/TelevisorApp.java
+++ src/TelevisorApp.java
@@ -1,5 +1,11 @@
 public class TelevisorApp {
     public static void main(String[] args) {
-        
+        Televisor tv = new Televisor();
+
+        tv.showStatus();
+        tv.turnOn();
+        tv.showStatus();
+        tv.turnOff();
+        tv.showStatus();
     }
 }
